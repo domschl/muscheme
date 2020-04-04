81cc71610aca568e7b50cecc18ba587d8b96e817
Dominik Schlösser <dsc@dosc.net>
2020-04-04 10:24:29 +0200

    rewritting completely

--- muscheme.cpp
+++ muscheme.cpp
@@ -1,10 +1,13 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <cassert>
 
-enum atom {nul, inum, fnum, string, symbol, proc, list};
+enum atom {nul, inum, fnum, str, symbol, proc, list};
 enum astval {valid, invalid, empty};
 
+typedef std::string string;
+auto NPOS=std::string::npos;
 
 struct astnode {
     atom type;
@@ -16,186 +19,93 @@ struct astnode {
 
 enum aststate {start, inter, token, end};
 
-astnode* parseToken(std::string tok) {
-    printf("[%s]", tok.c_str());
-    std::string iss="-0123456789";
+bool isnat(string tok) {
+    if (tok.length()==0) return false;
     std::string isn="0123456789";
-    std::string fss="-.0123456789";
-    if (tok.find("\"")!=std::string::npos) { // check string
-        if (tok[0]!='"' || tok[tok.length()-1]!='"') {
-            printf("Invalid string: %s",tok.c_str());
-            nast.state=astval::invalid;
-        } 
-        // more esc checks
-        nast.type=atom::string;
-        nast.state=astval::valid;
-        nast.u.str=tok.substr(1,tok.length()-2);
-        return nast;
+    for (int i=0; i<tok.length(); i++) {
+       if (isn.find(tok[i])==NPOS) return false;
+    }
+   return true; 
+}
+
+bool isint(string tok) {
+    printf("isint: %s\n",tok.c_str());
+    if (tok.length()==0) return false;
+    if (tok[0]=='-') return isnat(tok.substr(1));
+    return isnat(tok);
+}
+
+bool issimpfloat(string tok) {
+    printf("Simpfloat: %s\n",tok.c_str());
+    if (tok.length()==0) return false;
+    int p1=tok.find('.');
+    if (p1!=NPOS) {
+        return isint(tok.substr(0,p1)+tok.substr(p1+1));
     } else {
-        if (fss.find(tok[0]) == std::string::npos) { // check symbol
-            nast.type=atom::symbol;
-            nast.state=astval::valid;  // XXX checks
-            nast.u.str=tok;
-        return nast;
+        return isint(tok);
+    }
+}
+ 
+bool isfloat(string tok) {
+    if (tok.length()==0) return false;
+    if (isint(tok)) return true;
+    int p1=tok.find('e');
+    if (p1==NPOS) p1=tok.find('E');
+    if (p1==NPOS) {
+        return issimpfloat(tok);
+    } else {
+        return issimpfloat(tok.substr(0,p1)) && isint(tok.substr(p1+1));
+    }
+}
+
+int testit() {
+    int errs=0;
+    std::vector<string> ints{"0","01","23424","02020202","-1","-0","-923432"};
+    for(auto const& it: ints) {
+        if (!isint(it)) {
+            printf("Fail: %s is int!\n",it.c_str());
+            ++errs;
         } else {
-            if (tok.find('.') != std::string::npos) { // check float
-                nast.type=atom::fnum;
-                nast.state=astval::valid;  // XXX checks
-                nast.u.fnum=atof(tok.c_str());
-                return nast;
-            } else { // check int
-                nast.type=atom::inum;
-                nast.state=astval::valid;  // XXX checks
-                nast.u.inum=atoi(tok.c_str());
-                return nast;
-            }
+            printf("OK: %s is int.\n",it.c_str());
         }
     }
-}
-
-astlist *split(std::string cmd) {
-    astlist *vlist = new astlist();
-    astlist *empty = new astlist();
-    aststate state=aststate::start;
-    std::string ws=" \t\n\r";
-    std::string wse=" \t\n\r)(";
-    std::string curtok;
-    for (int i=0; i<cmd.length(); i++) {
-        printf("%c",cmd[i]);
-        switch (state) {
-            case aststate::token:
-                printf("<t>");
-                if (wse.find(cmd[i])!=std::string::npos) {
-                    ast el=parseToken(curtok);
-                    if (el.state==astval::valid) {
-                        vlist->push_back(el);
-                    }
-                    printf("%s->i>",el.u.str);
-                    state=aststate::inter; 
-                } else {
-                    curtok+=cmd[i];
-                }
-                break;
-            case aststate::inter:
-                printf("<i>");
-                if (ws.find(cmd[i])!=std::string::npos) break;
-                if (cmd[i]==')') {
-                    return vlist;
-                }
-                if (cmd[i]=='(') {
-                    std::string substr="";
-                    bool subok=false;
-                    int lv=0;
-                    char lc=' ';
-                    bool strstate=false;
-                    for (int j=i; j<cmd.length(); j++) {
-                        substr+=cmd[j];
-                        if (!strstate) {
-                            if (cmd[j]=='(') ++lv;
-                            if (cmd[j]==')') --lv;
-                            if (cmd[j]=='"' && lc!='\\') strstate=true;
-                        } else {
-                            if (cmd[j]=='"' && lc!='\\') strstate=false;
-                        }
-                        lc=cmd[j];
-                        if (lv==0) {
-                            subok=true;
-                            ast el; //={atom::nul, astval::invalid, 0};
-                            el.type=atom::list;
-                            el.vlist=split(substr);
-                            if ((el.vlist)->size()>0) {
-                                el.state=astval::valid;
-                                vlist->push_back(el);
-                            } else {
-                                printf("Parsing failed with sub-expression: %s", substr.c_str());
-                                //free(pel);
-                                return empty;
-                            }
-                            i==j;
-                            printf("->i>");
-                            state=aststate::inter; 
-                            break;
-                        }
-                    }
-                    printf("%s INVAL SUBEX\n",cmd.substr(i).c_str());
-                    return empty;
-                }
-                curtok=cmd[i];
-                printf("->t>");
-                state=aststate::token; 
-                break;
-            case aststate::start:
-                printf("<s>");
-                if (cmd[i]=='(') {
-                    printf("->i>");
-                    state=aststate::inter;
-                } else {
-                    printf("%s <> %s, expected '('", cmd.substr(0,i).c_str(), cmd.substr(i).c_str());
-                    return empty;
-                }
-                break;
-        }
-    }
-    return vlist;
-}
-
-void printAstlist(astlist *al) {
-    for (int i=0; i<al->size(); i++ ) {
-        if ((*al)[i].state==astval::invalid) {
-            printf("INV ");
-        } else if ((*al)[i].state==astval::empty) {
-            printf("EMPTY ");
+    std::vector<string> nints{"0e","0.1","23a424","","-",".","e"}; 
+    for (auto const& it: nints) {
+        if (isint(it)) {
+            printf("Fail: %s is NOT an int!\n",it.c_str());
+            ++errs;
         } else {
-            switch ((*al)[i]->type) {
-                case atom::fnum:
-                    printf("fnum %f ",(*al)[i].fnum);
-                    break;
-                case atom::inum:
-                    printf("inum %d ",(*al)[i].inum);
-                    break;
-                case atom::symbol:
-                    printf("sym %s",(*al)[i].str);
-                    break;
-                case atom::string:
-                    printf("string %s ",(*al)[i].str);
-                    break;
-                case atom::list:
-                    printf(" ( ");
-                    printAstlist((*al)[i].vlist);
-                    printf(" ) ");
-                    break;
-                default:
-                    printf("TYPEERR %d ",(*al)[i].type);
-                    break;
-            }
+            printf("OK: %s is not an int.\n",it.c_str());
         }
     }
-}
-
-std::string eval(std::string cmd) {
-    astlist *vlist=split(cmd);
-    printAstlist(vlist);
-    printf("\n");
-    return "";
-}
-
-void repl() {
-    std::string prompt="μλ>";
-    std::string line;
-    std::string resp;
-    bool esc=false;
-    while (!esc) {
-        std::cout << prompt;
-        std::cin >> line;
-        if (line=="(quit)") esc=true;
-        else {
-            resp=eval(line);
-            // std::cout << "\n-> " + resp +"\n";
+   std::vector<string> floats{"0","01","23424","02020202","-1","-0","-923432",".1","0.","-.1","-0.234","134.3233", ".0e4", "0e4","-1e3", "1E4", "1E-4", "-1.343e-32"};
+    for(auto const& it: floats) {
+        if (!isfloat(it)) {
+            printf("Fail: %s is float!\n",it.c_str());
+            ++errs;
+        } else {
+            printf("OK: %s is float.\n",it.c_str());
         }
     }
+    std::vector<string> nfloats{"0e",".e10","-e23a424","","-",".","e","1.43.322","1e1e1","1e1.2"}; 
+    for (auto const& it: nfloats) {
+        if (isfloat(it)) {
+            printf("Fail: %s is NOT a float!\n",it.c_str());
+            ++errs;
+        } else {
+            printf("OK: %s is not a float.\n",it.c_str());
+        }
+    }
+    return errs;
 }
 
 int main(int argc, char *argv[]) {
-    repl();
-    return 0;
+    int errs=testit();
+    if (errs==0) {
+        printf("All tests passed!\n");
+        return 0;
+    } else {
+        printf("%d tests failed!\n",errs);
+        return -1;
+    }
 }
