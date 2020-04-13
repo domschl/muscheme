dc9216f1deb0c797e21d7ab77484a1ef5ac6c6ef
Dominik Schlösser <dsc@dosc.net>
2020-04-13 12:56:08 +0200

    Multiline repl input

--- muscheme.cpp
+++ muscheme.cpp
@@ -6,14 +6,29 @@
 //
 #include "muscheme.h"
 
-void repl(std::string prompt="μλ> ") {
-    std::string cmd;
+void repl(std::string prompt="μλ> ", std::string prompt2="  > ") {
+    std::string cmd,inp;
     Muscheme ms;
+    bool fst;
     std::vector<Muscheme::astnode *> ast;
     while (true) {
-        std::cout << prompt;
-        getline(std::cin, cmd);
-        printf("%s\n",cmd.c_str());
+        cmd="";
+        fst=true;
+        while (true) {
+            if (fst) {
+                fst=false;
+                std::cout << prompt;
+            } else {
+                std::cout << prompt2;
+            }
+            getline(std::cin, inp);
+            if (inp!="") {
+                cmd += inp+"\n";
+                continue;
+            }
+            break;
+        }
+        printf("Inp: %s\n",cmd.c_str());
         if (cmd=="(quit)") return;
         ast=ms.parse(cmd);
         for (auto ap : ast) {
